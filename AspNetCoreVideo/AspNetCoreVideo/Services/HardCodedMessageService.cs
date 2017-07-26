206d5c985e16196484ef239c582f3cd3997bb968
rgvcycling <garza.r.tx@gmail.com>
2017-07-26 14:50:35 -0500

    Chapter one code

---
+++ AspNetCoreVideo/AspNetCoreVideo/Services/HardCodedMessageService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreVideo.Services
+{
+    public class HardCodedMessageService : IMessageService
+    {
+        public string GetMessage()
+        {
+            return "Hardcoded message from a service";
+        }
+    }
+}
---
+++ AspNetCoreVideo/AspNetCoreVideo/Services/IMessageService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreVideo.Services
+{
+    public interface IMessageService
+    {
+        string GetMessage();
+    }
+}
