ee2bd2f5cdde02291a3bbe80aad9752ed3672ff4
rgvcycling <garza.r.tx@gmail.com>
2017-07-30 16:55:30 -0500

    The end of Chapter 2

--- Chapter2/Chapter2/Controllers/HomeController.cs
+++ Chapter2/Chapter2/Controllers/HomeController.cs
@@ -4,16 +4,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Chapter2.Services;
 
 namespace Chapter2.Controllers
 {
+    
     public class HomeController : Controller
     {
+        private IVideoData _videos;
+        public HomeController(IVideoData videos)
+        {
+            _videos = videos;
+        }
+
         public ViewResult Index()
         {
-            var model = new Video { Id = 1, Title = "Shreck" };
+            var model = _videos.GetAll();
 
             return View(model);
         }
+
     }
 }
---
+++ Chapter2/Chapter2/Services/IVideoData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Chapter2.Models;
+
+namespace Chapter2.Services
+{
+    public interface IVideoData
+    {
+        IEnumerable<Video> GetAll();
+
+    }
+}
