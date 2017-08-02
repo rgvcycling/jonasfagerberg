dea2ff67b9e11165b61c05ec3b08c8c46122d2c8
rgvcycling <garza.r.tx@gmail.com>
2017-08-02 11:58:15 -0500

    Added data annotations and validations

--- Chapter3/Chapter3/Entities/Video.cs
+++ Chapter3/Chapter3/Entities/Video.cs
@@ -1,6 +1,7 @@
 ﻿using Chapter3.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,11 @@ namespace Chapter3.Entities
     public class Video
     {
         public int Id { get; set; }
+
+        [Required, MinLength(3), MaxLength(80), DataType(DataType.Password)]
         public string Title { get; set; }
+
+        [Display(Name ="Film Genre")]
         public Genres Genre { get; set;  }
     }
 }
--- Chapter3/Chapter3/ViewModels/VideoEditViewModel.cs
+++ Chapter3/Chapter3/ViewModels/VideoEditViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Chapter3.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,11 @@ namespace Chapter3.ViewModels
     public class VideoEditViewModel
     {
         public int id { get; set; }
+
+        [Required, MinLength(3), MaxLength(80)]
         public string Title { get; set; }
+
+        [Display(Name ="Film Genre")]
         public Genres Genre { get; set; }
     }
 }
