3ac0ebc718a44e70276dc060989f507a243bb00a
rgvcycling <garza.r.tx@gmail.com>
2017-08-02 16:50:27 -0500

    addedEntity Framework and VideoDbContext class

---
+++ Chapter4/Chapter4/Chapter4/Data/VideoDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Chapter4.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter4.Data 
+{
+    public class VideoDbContext : DbContext
+    {
+        public DbSet<Video> Videos { get; set; }
+        public VideoDbContext(DbContextOptions<VideoDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
