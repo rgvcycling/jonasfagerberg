85a0704bdb9b5bc1ec33f2a55c22149912bac6f9
rgvcycling <garza.r.tx@gmail.com>
2017-08-28 16:47:11 -0500

    Added the methods for the Adim menu. Need UserCourse Edit and Delete methods

---
+++ VideoOnDemand/VideoOnDemand/Controllers/UserCoursesController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoOnDemand.Data;
+using VideoOnDemand.Models;
+using VideoOnDemand.Models.DTOModels;
+
+namespace VideoOnDemand.Controllers
+{
+    public class UserCoursesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserStore<ApplicationUser, IdentityRole, ApplicationDbContext> _userStore;
+        
+        public UserCoursesController(UserStore<ApplicationUser, IdentityRole, ApplicationDbContext> userStore)
+        {
+            _db = userStore.Context;
+            _userStore = userStore;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _db.Courses
+                .Join(_db.UserCourses, c => c.Id, uc => uc.CourseId, (c, uc) => new { Courses = c, UserCourses = uc })
+                .Select(s => new UserCourseDTO
+                {
+                    CourseId = s.Courses.Id,
+                    CourseTitle = s.Courses.Title,
+                    UserId = s.UserCourses.UserId,
+                    UserEmail = _userStore.Users.FirstOrDefault(u => u.Id.Equals(s.UserCourses.UserId)).Email
+                });
+
+            return View(model);
+        }
+        public async Task<IActionResult> Details(string userId, int courseId)
+        {
+            if (userId == null || courseId.Equals(default(int)))
+            {
+                return NotFound();
+            }
+            var model = await _db.Courses
+             .Join(_db.UserCourses, c => c.Id, uc => uc.CourseId, (c, uc) => new { Courses = c, UserCourses = uc })
+             .Select(s => new UserCourseDTO
+             {
+                 CourseId = s.Courses.Id,
+                 CourseTitle = s.Courses.Title,
+                 UserId = s.UserCourses.UserId,
+                 UserEmail = _userStore.Users.FirstOrDefault(u => u.Id.Equals(s.UserCourses.UserId)).Email
+             })
+             .FirstOrDefaultAsync(w => w.CourseId.Equals(courseId) && w.UserId.Equals(userId));
+            
+            if ( model == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+        public IActionResult Create()
+        {
+            // Populate the Course ID dropdown selector
+            ViewData["CourseId"] = new SelectList(_db.Courses, "Id", "Title");
+
+            // Populate the User ID dropdown selector
+            ViewData["UserId"] = new SelectList(_userStore.Users, "Id", "Email");
+
+            return View();
+        }
+    }
+}
--- VideoOnDemand/VideoOnDemand/Startup.cs
+++ VideoOnDemand/VideoOnDemand/Startup.cs
@@ -42,6 +42,7 @@ namespace VideoOnDemand
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<UserStore<ApplicationUser, IdentityRole, ApplicationDbContext>>();
             // Add framework services.
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
